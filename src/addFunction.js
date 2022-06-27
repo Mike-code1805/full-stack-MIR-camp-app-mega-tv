3f4e9e643e8e07ddd022a146075216008682bef7
Miguel Enrique Saca Accostupa <miguelenrique1805@gmail.com>
2022-06-26 21:50:34 -0500

    first component

--- src/addFunction.js
+++
@@ -1,9 +0,0 @@
-const addFunction = (...numbers) => {
-  let result = 0;
-  for (const number of numbers) {
-    result += number;
-  }
-  return result < 0 ? 0 : result;
-};
-
-module.exports = addFunction;
